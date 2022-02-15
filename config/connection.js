2cbeed9162d6ee677d1b3943d755633e16752eaf
Mike <mikej.miller440@gmail.com>
2022-02-15 14:21:46 -0600

    added api functions

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 // import the Sequelize constructor from the library
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
--- models/User.js
+++ models/User.js
@@ -7,11 +7,40 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
     {
-        // TABLE COLUMN DEFINITIONS GO HERE
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
-
-        // pass in our imported sequelize connection (the direct connection to our )
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
-)
+);
+
+module.exports = User;
